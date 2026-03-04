ec8a2d3c622c6661d426cc86218217db47da46d6
felipenascimento157-ui <felipe_nascimento157@estudante.sesisenai.org.br>
2026-03-04 20:16:51 +0000

    update

--- ATIVIDADE python/ATIVIDADE6.py
+++ ATIVIDADE python/ATIVIDADE6.py
@@ -1,8 +1,8 @@
 nome = (input ("insira um nome "))
 idade = int (input ("insira idade( anos completos: "))
 
-#repete quando a idade for valida
+#repete quando a idade for valida 
 while idade > 120 or idade < 0:
- idade = int(input("idade(anos completos -ate 120 ano):"))
- dias_vidas = idade * 36
- print (f"") 
+   print = int(input("idade(anos completos -ate 120 ano):"))
+dias_vidas = idade * 365
+print (f"{nome}, voce viveu {dias_vidas}")
